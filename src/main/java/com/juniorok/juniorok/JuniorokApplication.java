1ff82babdfe5f2e28e124b839cc8e4ed0646007a
Seongguk Jeong <seongguk.dev@gmail.com>
2023-08-14 14:36:40 +0900

    Spring 프로젝트 추가

---
+++ src/main/java/com/juniorok/juniorok/JuniorokApplication.java
@@ -0,0 +1,13 @@
+package com.juniorok.juniorok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuniorokApplication.class, args);
+    }
+
+}
