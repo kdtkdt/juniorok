d64379657ed43a8a01768b61446949b271ed276e
정성국 <func.one@gmail.com>
2023-08-17 16:13:52 +0900

    refactor: static file에 대한 스프링 시큐리티 설정 해제
    static file에 대해 불필요한 스프링 시큐리티 보안 작업을 수행하지 않도록 파일을 이동하고, 설정을 추가합니다.

--- src/main/java/com/juniorok/juniorok/config/SecurityConfig.java
+++ src/main/java/com/juniorok/juniorok/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.juniorok.juniorok.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -12,8 +14,15 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
 
     @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/img/**", "/css/**", "/js/**");
+    }
+
+    @Bean
     SecurityFilterChain pageRequestFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(auth -> {auth.requestMatchers(new AntPathRequestMatcher("/**")).permitAll();})
+                .oauth2Login(Customizer.withDefaults())
                 .build();
     }
 }
