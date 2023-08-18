b413a3ac5505d2bb27e812cf5c10b91e58f7b3a5
정성국 <func.one@gmail.com>
2023-08-18 22:23:22 +0900

    fix: 관리자 및 작성자에게 공고 작성 버튼 보이지 않음
    Github 로그인 시에는 무조건 USER 역할이 부여되도록 되어있어, ADMIN과 WRITER 역할이 있는 사용자에게 공고 작성 버튼이 보이지 않았습니다. 그래서 Github 로그인 시에도 사용자에게 역할이 정상적으로 부여되도록 수정하였습니다.

--- src/main/java/com/juniorok/juniorok/service/UserAuthService.java
+++ src/main/java/com/juniorok/juniorok/service/UserAuthService.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -56,15 +58,6 @@ public class UserAuthService extends DefaultOAuth2UserService implements UserDet
                 new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
     }
 
-    private String getUserRole(User user) {
-        if (user.isAdmin())
-            return "ROLE_ADMIN";
-        else if (user.isWriter())
-            return "ROLE_WRITER";
-        else
-            return "ROLE_USER";
-    }
-
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
@@ -73,8 +66,28 @@ public class UserAuthService extends DefaultOAuth2UserService implements UserDet
             email = Optional.of(getUserEmail(userRequest));
         Map<String, Object> attributes = new LinkedHashMap<>(oAuth2User.getAttributes());
         attributes.put("email", email.get());
+        List<GrantedAuthority> authorityList = getUserAuthorityList(oAuth2User);
         saveUser(attributes);
-        return new DefaultOAuth2User(oAuth2User.getAuthorities(), attributes, GITHUB_NAME_ATTRIBUTE_KEY);
+        return new DefaultOAuth2User(authorityList, attributes, GITHUB_NAME_ATTRIBUTE_KEY);
+    }
+
+    private List<GrantedAuthority> getUserAuthorityList(OAuth2User oAuth2User){
+        List<GrantedAuthority> authorityList = AuthorityUtils.createAuthorityList();
+        Optional<User> user = userRepository.findByName(oAuth2User.getAttributes().get(GITHUB_NAME_ATTRIBUTE_KEY).toString());
+        if (user.isPresent()) {
+            authorityList.addAll(oAuth2User.getAuthorities());
+            authorityList.add(new SimpleGrantedAuthority(getUserRole(user.get())));
+        }
+        return authorityList;
+    }
+
+    private String getUserRole(User user) {
+        if (user.isAdmin())
+            return "ROLE_ADMIN";
+        else if (user.isWriter())
+            return "ROLE_WRITER";
+        else
+            return "ROLE_USER";
     }
 
     private String getUserEmail(OAuth2UserRequest userRequest) {
