008a6d3e8ed034e5933f622ee9a4fe6afa60fd6a
mareox31 <mare.ox.31@gmail.com>
2023-08-23 14:31:32 +0900

    feat : 공고상세페이지 지도api추가
    1.근무지로 검색되게 되어있습니다.
    2.지도에 마커 누르면 다음지도로 이동합니다.
    3.이동한 지도에 주소로 검색이 된 결과라 회사이름이 뜨지 않습니다.
    
    **추후 마커에 회사이름 뜨도록 수정 예정.

--- src/main/java/com/juniorok/juniorok/controller/BoardController.java
+++ src/main/java/com/juniorok/juniorok/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.juniorok.juniorok.controller;
 
+import com.juniorok.juniorok.config.ApiConfig;
 import com.juniorok.juniorok.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 public class BoardController {
 
     private final PostService postService;
+    private final ApiConfig apiConfig;
 
     @GetMapping({"/main", "/"})
     public String showMainBoard() {
@@ -22,6 +24,7 @@ public class BoardController {
     @GetMapping("/mypage")
     public String showMyPage(Model model) {
         model.addAttribute("posts", postService.getPage(1, 10));
+        model.addAttribute("kakaoMapAppkey", apiConfig.getKakaoMapAppkey());
         return "mypage";
     }
 
